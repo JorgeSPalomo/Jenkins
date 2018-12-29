9adf08f63e296778d398e315598b97bcf438dfa6
JorgeSPalomo <JorgeSPalomo@yahoo.com>
2018-12-29 11:50:36 -0500

    2nd commit

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -1,4 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class BaseClass {
 
+	@Test
+	public void Jorge() {
+
+		System.setProperty("webdriver.chrome.driver",
+				"src/test/resources/Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String url = "https://www.google.com/";
+		driver.get(url);
+		String title = driver.getTitle();
+
+		Assert.assertEquals("Google", title);
+
+		driver.quit();
+	}
+	
+	public static void main(String[]args) {
+		
+	}
 }
